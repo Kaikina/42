e3e373efa682f9f1d79e1979879dd7c64119f648
Tom GIROU <tgirou@student.42.fr>
2015-09-07 22:22:29 +0200

    volume

--- troll.sh
+++ troll.sh
@@ -1,6 +1,6 @@
-osascript -e "set volume 5"
+osascript -e "set volume 7"
 while (true) do
-osascript -e "set volume 5"
+osascript -e "set volume 7"
 osascript -e 'beep'
 say I didn\'t lock my fucking session
 done
