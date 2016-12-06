53fc2bfdd9ffc47defd594ae86617e88c2c36054
Tom GIROU <tgirou@e2r9p12.42.fr>
2016-12-06 08:53:33 +0100

    ajout fonction ft_strpbrk

--- Libft/libft.h
+++ Libft/libft.h
@@ -6,7 +6,7 @@
 /*   By: tgirou <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/11/08 10:33:47 by tgirou            #+#    #+#             */
-/*   Updated: 2016/12/06 07:18:39 by tgirou           ###   ########.fr       */
+/*   Updated: 2016/12/06 08:52:24 by tgirou           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,6 +14,7 @@
 # define LIBFT_H
 # include <string.h>
 
+char				*ft_strpbrk(const char *str1, const char *str2);
 void				ft_printmatrice(char **tab);
 void				*ft_memset(void *b, int c, size_t len);
 void				ft_bzero(void *s, size_t n);
